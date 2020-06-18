a58a08180ba2ddda717753ae99fe0a3d7fe41085
Aditya Pandey <aditya.pandey_bca17@gla.ac.in>
2020-06-18 19:58:41 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -8,14 +8,14 @@ use Kreait\Firebase\ServiceAccount;
 
 // This assumes that you have placed the Firebase credentials in the same directory
 // as this PHP file.
-$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/loginsignup-2e506-firebase-adminsdk-h5xqt-e6c18654d0.json');
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/______JASON FILE______');
 
 $firebase = (new Factory)
     ->withServiceAccount($serviceAccount)
     // The following line is optional if the project id in your credentials file
     // is identical to the subdomain of your Firebase project. If you need it,
     // make sure to replace the URL with the URL of your project.
-    ->withDatabaseUri('https://loginsignup-2e506.firebaseio.com/')
+    ->withDatabaseUri('____DATABASE______')
     ->create();
 
 $database = $firebase->getDatabase();
